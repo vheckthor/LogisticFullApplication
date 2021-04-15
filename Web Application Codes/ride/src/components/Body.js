582308c5628d3cce5feaa21b2b2677364e13ae8f
unknown <tunarshreal@gmail.com>
2021-04-15 16:37:30 -0200

    edited the input width

--- Web Application Codes/ride/src/components/Body.js
+++ Web Application Codes/ride/src/components/Body.js
@@ -7,16 +7,16 @@ function Body() {
                 <h1>Select Ride Type</h1>
                 <form>
                     <input type='text' placeholder='enter you pick up location' />
-                    <input type='text' placeholder='enter you destination'/><br/>
-                    <input type='submit' value='order now'/>
+                    <input type='text' placeholder='enter you destination' className='destination'/><br/>
+                    <input type='submit' value='order now' className='submit'/>
                 </form>
             </div>
             <div className='maps'>
-            <h1>Select Ride Type</h1>
+            {/* <h1>Select Ride Type</h1>
                 <form>
                     <input type='text' placeholder='enter you pick up location' />
                     <input type='text' placeholder='enter you destination'/>
-                </form>
+                </form> */}
             </div>
         </div>
     )
