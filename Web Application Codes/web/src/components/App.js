8da5513a35b6daa3b139b8fc13decca4e89a22a8
Joshua Ayodele <jayodele64@gmail.com>
2021-05-06 22:52:01 +0100

    add web folder

---
+++ Web Application Codes/web/src/components/App.js
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+import Nav from './Nav';
+import Home from './Home';
+import SignUp from './SignUp';
+import Footer from './Footer';
+import LogIn from './Login';
+import '../css/index.css';
+
+function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/'>
+					<Nav />
+					<Home />
+					<Footer />
+				</Route>
+				<Route exact path='/register'>
+					<SignUp />
+				</Route>
+				<Route exact path='/login'>
+					<LogIn />
+				</Route>
+				<Route exact path='/ride'>
+					<h1>Rides Components go here</h1>
+					<Link to='/'>
+						<button>Home</button>
+					</Link>
+					<Link to='/register'>
+						<button>Register</button>
+					</Link>
+				</Route>
+				<Route>
+					<h1>Nothing is here</h1>
+				</Route>
+			</Switch>
+		</Router>
+	);
+}
+
+export default App;
