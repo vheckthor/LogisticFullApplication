da78c7d0e1afdebbb2c7e3c5551dfa89b424f10c
Joshua Ayodele <jayodele64@gmail.com>
2021-09-11 17:33:49 +0100

    add component for sign up page

--- Web Application Codes/web/src/components/SignUpOption.js
+++ Web Application Codes/web/src/components/SignUpOption.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Container, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { useHistory, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import Nav from './Nav';
 import RafkiSVG from './RafkiSVG';
@@ -30,7 +30,6 @@ function SignUpOption() {
 		},
 	}));
 	const classes = useStyles();
-	let history = useHistory();
 
 	return (
 		<div className='wrapper'>
