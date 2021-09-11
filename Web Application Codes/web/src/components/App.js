b7efc4dc781f9d454629829294119fbeaba50e43
Joshua Ayodele <jayodele64@gmail.com>
2021-09-11 17:32:05 +0100

    modify route handler for signup

--- Web Application Codes/web/src/components/App.js
+++ Web Application Codes/web/src/components/App.js
@@ -4,9 +4,10 @@ import Home from './Home';
 import SignUpOld from './SignUp_Old';
 import LogIn from './Login';
 import Welcome from './Welcome';
-import Footer from './Footer'
-import SignUp from './SignUp'; 
-import Error from './Error.js'
+import Footer from './Footer';
+import SignUp from './SignUp';
+import Error from './Error.js';
+import SignUpOption from './SignUpOption';
 // import ErrorPage from './ErrorPage'
 
 function App() {
@@ -17,20 +18,29 @@ function App() {
 					<Home />
 					<Footer />
 				</Route>
+
+				<Route exact path='/sign_up'>
+					<SignUpOption />
+					{/* <SignUp /> */}
+				</Route>
+
+				<Route exact path='/sign_up/rider'>
+					<SignUp />
+				</Route>
+
 				<Route path='/register'>
 					<SignUpOld />
 				</Route>
+
 				<Route path='/login'>
 					<LogIn />
 				</Route>
+
 				<Route path='/ride'>
 					<Welcome />
 				</Route>
 
-				<Route path='/sign_up'>
-					<SignUp />
-				</Route>
-				<Route >
+				<Route>
 					<Error />
 				</Route>
 				{/* <Route>
