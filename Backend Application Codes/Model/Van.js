59700c571c78f7b618deb1bf6539ec8c2918e69b
Daso <daso.timileyin@gmail.com>
2022-01-18 13:37:30 +0100

    startup fixes

--- Backend Application Codes/Model/Van.js
+++ Backend Application Codes/Model/Van.js
@@ -4,70 +4,69 @@ const vanSchema = new Schema(
     {
         individualName: {
             type: String,
-            required: true,
+            
         },
         companyName: {
             type: String,
-            required: true,
+            
         },
         companySignupCode: {
             type: String,
-            required: true,
+            
         },
         type: {
             typr: String,
-            required: true
         },
         driverName: {
             type: String,
-            required: true,
+            
         }, /// { type: Schema.Types.ObjectId, ref: 'Rider' },
         driverIdNumber: {
             type: String,
-            required: true,
+            
         },
         driverDrivingLicense: {
             type: String,
-            required: true,
+            
         },
         vanPlateNumber: {
             type: String,
-            required: true,
+            
         },
         vehicleLicense: {
             type: String,
-            required: true,
+            
         },
         hackenypermit: {
             type: String,
-            required: true,
+            
         },
         roadWorthiness: {
             type: String,
-            required: true,
+            
         },
         insurance: {
             type: String,
-            required: true,
+            
         },
         comprehensiveInsurance: {
             type: String,
         },
         goodsOnly: {
             type: String,
-            required: true,
+            
         },
         ikdUnifiedByePass: {
             type: String,
-            required: true,
+            
         },
         GITInsurance: {
             type: String,
-            required: true,
+            
         },
         allocationOfPlateNumber: {
             type: String,
-            required: true,
+            
         },
         otherDocuments: {
             type: String,
@@ -85,7 +84,7 @@ const vanSchema = new Schema(
 
 // email: {
 //     type: String,
-//     required: true,
+//     
 //   },
 
 const Van = model('Vans', vanSchema);
